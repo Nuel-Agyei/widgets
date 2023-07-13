e4ce1346af0b6e206c62022b8421bc72a445caba
Nuel Agyei <nuelagyei992@gmail.com>
2023-07-13 15:28:20 +0000

    start

---
+++ app/Filament/Widgets/attendance.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class attendance extends Widget
+{
+    protected static string $view = 'filament.widgets.attendance';
+}
